f6e582e3de817cff5b05f1ab599cf9de3937e51e
Iluze Ear <nsumarna150@gmail.com>
2024-10-26 21:56:59 +0700

    Add js and materi

--- learn/javascript/script.js
+++ learn/javascript/script.js
@@ -1,3 +1,16 @@
-let judul = "Hello World";
-const idJudul = document.getElementById("judul");
-idJudul.innerHTML = judul;
+let Jeruk = 5;
+let Buah = 3;
+let Hayam = 7;
+let Words =
+  "Saya memilki Jeruk sebanyak " +
+  Jeruk +
+  " , dan Jeruk tergolong ke dalam kategori ke " +
+  Buah +
+  " dan saya memiliki seekor Ayam sebanyak " +
+  Hayam +
+  " ekor";
+alert(Words);
+
+let Nama = "Aditya";
+let Umur = 21;
+let Perempuan = false;
